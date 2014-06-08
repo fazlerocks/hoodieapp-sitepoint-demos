cf09f79a26bffb2ea9e0eb6bf687c6dad744f1f9
Syed Fazle Rahman <fazlerahman786@gmail.com>
2014-06-09 02:42:26 +0530

    Added new files

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+"use strict";
+var hoodie  = new Hoodie();
+
+$(document).ready(function () {
+
+    checkuser();
+
+    $("#registerForm").submit(function(e){
+        e.preventDefault();
+        var username = $("#username").val();
+        var password = $("#password").val();
+        hoodie.account.signUp(username, password).done(function (user) {
+            checkuser();
+        });
+    });
+
+    $("#signout").click(function () {
+        hoodie.account.signOut().done(function (user) {
+            checkuser();
+        });
+    });
+});
+
+function checkuser() {
+    if(hoodie.account.username){
+        $("#registrationPage").hide();
+        $("#userPage").show();
+        $("#userPage h2").text("Welcome, "+hoodie.account.username+"!");
+    }else{
+        $("#registrationPage").show();
+        $("#userPage").hide();
+        $("#userPage h2").text("");
+    }
+}
